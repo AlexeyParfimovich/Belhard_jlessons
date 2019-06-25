ef6999e3f8875263be63a81f240611a15eab827c
Alexey Parfimovich <alexey.parfimovich@yandex.by>
2019-06-25 09:04:16 +0300

    Решение задачи через регулярные выражения

--- src/homeWorks/Task18.java
+++ src/homeWorks/Task18.java
@@ -6,19 +6,29 @@ package homeWorks;
 */
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task18 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("\nPlease, input a string with different punctuation marks:");
-
+        System.out.println("Please, input a string with different punctuation marks:");
         String str = sc.nextLine();
         sc.close();
 
+        Pattern regexp = Pattern.compile("[,.:;!?-]");
+        Matcher mstr = regexp.matcher(str);
+
         int marksNum = 0;
 
+        // Поиск знаков препинания - через регулярные выражения
+        while(mstr.find()) {
+            marksNum++;
+        }
+/*
+        // Поиск знаков препинания - не оптимальный вариант методом перебора всех символов строки
         for(int i = 0;i < str.length(); i++){
 
             switch (str.charAt(i)) {
@@ -34,6 +44,7 @@ public class Task18 {
                 case '"' : marksNum++; break;
             };
         }
+*/
 
         System.out.println("\nThere are " + marksNum + " different punctuation marks in the string");
     }
